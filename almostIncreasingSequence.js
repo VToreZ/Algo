ad0b5df20378a7580297ae9ac44fd3c981d7d7b8
VToreZ <lumisitone@gmail.com>
2019-10-08 11:40:35 +0300

    + 1 question

--- almostIncreasingSequence.js
+++ almostIncreasingSequence.js
@@ -1,14 +1,21 @@
-function almostIncreasingSequence(sequence) {
+function almostIncreasingSequence(a) {
   let excessCount = 0;
-  for (let i = 0; i < sequence.length; i++) {
-    if ((sequence[i] <= Math.max(...sequence.slice(0, i))) || (sequence[i] >= Math.max(...sequence.slice(i, sequence.length)))) {
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] >= a[i + 1]) {
+      console.log('bad: ' + a[i]);
       excessCount += 1;
-      sequence.splice(i, 1);
-      console.log(sequence);
-      i--;
+    }
+    if (excessCount > 1) {
+      return false;
+    }
+    if (a[i + 2] <= a[i] && a[i + 1] <= a[i - 1]) {
+      return false;
     }
   }
-  return excessCount <= 1;
+  return true;
 }
+// Проверка через одного
 
-console.log(almostIncreasingSequence([10, 1, 2, 3, 4, 5]));
+console.log(almostIncreasingSequence([1, 2, 3, 4, 3, 6])); //false
+console.log(almostIncreasingSequence([1, 2, 1, 2]));  // false
+console.log(almostIncreasingSequence([10, 1, 2, 3, 4, 5, 6])); // true
